86d3e5c0cd3570df739c43ea43adc645ca70308a
vipe7 <bukoreshtliev@gmail.com>
2020-07-10 01:47:28 +0300

    Updates

--- Install.ps1
+++
@@ -1,5 +0,0 @@
-curl -o $env:userprofile\desktop\Vipers.zip https://www.dropbox.com/s/f9zvh7z3we3ntnt/Install.zip?dl=1
-Expand-Archive -Force $env:userprofile\desktop\Vipers.zip C:\
-Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt | Out-String
-Remove-Item $env:userprofile\desktop\Vipers.zip -Force -Recurse
-Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
--- Update.ps1
+++ Update.ps1
@@ -1,12 +1,12 @@
 New-Item -ItemType "directory" -Path "$env:userprofile\desktop\Vipers-Backup" -Force | Out-Null
 Copy-Item -Path "C:\Rainmeter\Skins\Vipers\@Resources\*txt" -Destination "$env:userprofile\desktop\Vipers-Backup"
 Stop-Process -Name "Rainmeter"
-curl -o C:\Rainmeter\Update.zip https://github.com/vipe7/Vipers/raw/master/Update.zip
-Remove-Item C:\Rainmeter\Skins\Vipers -Force -Recurse
-Expand-Archive -Force C:\Rainmeter\Update.zip C:\Rainmeter\Skins
+curl -o $env:userprofile\desktop\Install.zip https://github.com/vipe7/Vipers/raw/master/Vipers.zip
+Remove-Item C:\Rainmeter -Force -Recurse
+Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\
 Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\*txt" -Destination "C:\Rainmeter\Skins\Vipers\@Resources"
-Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt | Out-String
 Remove-Item $env:userprofile\desktop\Vipers-Backup -Force -Recurse
-Remove-Item C:\Rainmeter\Update.zip -Force -Recurse
+Remove-Item $env:userprofile\desktop\Install.zip -Force -Recurse
 Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
+Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt | Out-String
 Pause
