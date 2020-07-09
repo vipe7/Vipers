6b9f860ca2511572262bef5e9265be869652ca19
vipe7 <bukoreshtliev@gmail.com>
2020-07-09 15:40:12 +0300

    Update Update.ps1

--- Update.ps1
+++ Update.ps1
@@ -5,7 +5,7 @@ curl -o C:\Rainmeter\Update.zip https://www.dropbox.com/s/k3svqast39kel8x/Update
 Remove-Item C:\Rainmeter\Skins\Vipers -Force -Recurse
 Expand-Archive -Force C:\Rainmeter\Update.zip C:\Rainmeter\Skins
 Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\*txt" -Destination "C:\Rainmeter\Skins\Vipers\@Resources"
-Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt | Out-String
+Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt
 Remove-Item $env:userprofile\desktop\Vipers-Backup -Force -Recurse
 Remove-Item C:\Rainmeter\Update.zip -Force -Recurse
 Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
