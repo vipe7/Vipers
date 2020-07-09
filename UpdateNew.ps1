85fdb9347b1caeac33ba44b3f8a61d54d580bf6b
vipe7 <bukoreshtliev@gmail.com>
2020-07-10 01:40:33 +0300

    Update UpdateNew.ps1

--- UpdateNew.ps1
+++ UpdateNew.ps1
@@ -3,7 +3,7 @@ Copy-Item -Path "C:\Rainmeter\Skins\Vipers\@Resources\*txt" -Destination "$env:u
 Stop-Process -Name "Rainmeter"
 curl -o $env:userprofile\desktop\Install.zip https://github.com/vipe7/Vipers/raw/master/Vipers.zip
 Remove-Item C:\Rainmeter -Force -Recurse
-Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\Rainmeter
+Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\
 Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\*txt" -Destination "C:\Rainmeter\Skins\Vipers\@Resources"
 Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt | Out-String
 Remove-Item $env:userprofile\desktop\Vipers-Backup -Force -Recurse
