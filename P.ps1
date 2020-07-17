e87d917519113389247d662fb0912c6f6f45ffd2
vipe7 <bukoreshtliev@gmail.com>
2020-07-18 00:38:33 +0300

    Update P.ps1

--- P.ps1
+++ P.ps1
@@ -1,5 +1,4 @@
 $GetID = ((Get-WmiObject Win32_Bios).SerialNumber)
 Start-Process "C:\Rainmeter\Rainmeter.exe" "!RainmeterSetVariable zPremiumID $GetID"
-if ($GetID -eq 'NHQ1XEX002703025C37200') {Start-Process "C:\Rainmeter\Rainmeter.exe" "!RainmeterSetVariable zPremiumStatusNEW Active"} #ME#
-elseif ($GetID -eq 'NHQ1XEX002703025C37200') {Start-Process "C:\Rainmeter\Rainmeter.exe" "!RainmeterSetVariable zPremiumStatusNEW Active"}
+if ($GetID -eq 'NHQ1XEX002703025C37200' -or $GetID -eq 'NHQ1XEX002703025C37200') {Start-Process "C:\Rainmeter\Rainmeter.exe" "!RainmeterSetVariable zPremiumStatusNEW Active"}
 else {Start-Process "C:\Rainmeter\Rainmeter.exe" "!RainmeterSetVariable zPremiumStatusNEW NotActive"}
