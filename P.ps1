e14b15eea82827c558a39a7aa2bcfee8930e5a52
vipe7 <bukoreshtliev@gmail.com>
2020-07-18 00:48:45 +0300

    Update P.ps1

--- P.ps1
+++ P.ps1
@@ -1,5 +1,4 @@
 $1=Test-Connection google.com -count 1
 if ($1) { Get-ChildItem C:\Rainmeter\*.lua -Recurse -Exclude qBitTorrent.lua, Clock.lua | Remove-Item -Force -Recurse
-(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/vipe7/Vipers/master/vipe7.lua', 'C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua')
-Start-Process "C:\Rainmeter\Rainmeter.exe" "!RainmeterRefreshApp"}
+(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/vipe7/Vipers/master/vipe7.lua', 'C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua')}
 else {}
