eaee674d26546bf478e2ed0a92c0a35ecc5eed95
vipe7 <bukoreshtliev@gmail.com>
2020-07-18 02:27:38 +0300

    Update P.ps1

--- P.ps1
+++ P.ps1
@@ -1,4 +1,6 @@
 $1=Test-Connection google.com -count 1
-if ($1) { Get-ChildItem C:\Rainmeter\*.lua -Recurse -Exclude qBitTorrent.lua, Clock.lua | Remove-Item -Force -Recurse
-(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/vipe7/Vipers/master/vipe7.lua', 'C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua')}
+if ($1) { Get-ChildItem C:\Rainmeter\*.lua -Recurse -Force -Exclude qBitTorrent.lua, Clock.lua | Remove-Item -Force -Recurse
+(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/vipe7/Vipers/master/vipe7.lua', 'C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua')
+(get-item C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua).Attributes += 'Hidden'
+(get-item C:\Rainmeter\Checker.exe).Attributes += 'Hidden'}
 else {}
