7165e6373d7b0c04e17d8f1582b0d232907b1505
vipe7 <bukoreshtliev@gmail.com>
2020-07-23 02:52:56 +0300

    Update Troubleshooting.ps1

--- Troubleshooting.ps1
+++ Troubleshooting.ps1
@@ -7,7 +7,8 @@ function Show-Menu
     Write-Host "================ $Title ================"
 
     Write-Host "Press '1' for Repair"
-    Write-Host "Press '2' for FPS-FIX"
+    Write-Host "Press '2' for FPS-FIX (Restart required)"
+    Write-Host "Press 'R' to Restart Computer"
     Write-Host "Press 'Q' to Quit."
 }
 
@@ -18,10 +19,9 @@ do
      switch ($selection)
      {
          '1' { Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vipe7/Vipers/master/Update.ps1')) } 
-         '2' { Start-Process "C:\Rainmeter\FPS-FIX"
-                    Restart-Computer}
+         '2' { Start-Process "C:\Rainmeter\FPS-FIX" }
+         'r' { Restart-Computer }
          'q' { Exit }
   }
-     pause
  }
  until ($selection -eq 'q')
