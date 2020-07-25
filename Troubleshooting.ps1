ef1b5db19d43b4edb8313b08d380346780752946
vipe7 <bukoreshtliev@gmail.com>
2020-07-25 03:19:07 +0300

    Update Troubleshooting.ps1

--- Troubleshooting.ps1
+++ Troubleshooting.ps1
@@ -8,6 +8,7 @@ function Show-Menu
 
     Write-Host "Press '1' for Repair"
     Write-Host "Press '2' for FPS-FIX (Restart required)"
+    Write-Host "Press '3' for WebNowPlaying Configuration Guide"
     Write-Host "Press 'R' to Restart Computer"
     Write-Host "Press 'Q' to Quit."
 }
@@ -20,6 +21,7 @@ do
      {
          '1' { Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vipe7/Vipers/master/Update.ps1')) } 
          '2' { Start-Process "C:\Rainmeter\FPS-FIX" }
+		 '3' { Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vipe7/Vipers/master/WebNowPlaying.ps1')) }
          'r' { Restart-Computer }
          'q' { Exit }
   }
