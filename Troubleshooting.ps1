8762f6e71ff6953944110bc0bfe26da38a2e2093
vipe7 <bukoreshtliev@gmail.com>
2020-07-26 01:49:26 +0300

    Update Troubleshooting.ps1

--- Troubleshooting.ps1
+++ Troubleshooting.ps1
@@ -5,16 +5,8 @@ function Show-Menu
     )
     Clear-Host
     Write-Host "================ $Title ================"
-    Write-Host "Press '1' for Repair"
-    Write-Host "Press '2' for FPS-FIX (Restart required)"
-    Write-Host ""
-    Write-Host "================ Configuration  Guides ================"
-    Write-Host "Press '3' for Fraps (Under Construction)"
-    Write-Host "Press '4' for HWiNFO (Under Construction)"
-    Write-Host "Press '5' for qBitTorrent (Under Construction)"
-    Write-Host "Press '6' for WebNowPlaying"
-    Write-Host ""
-    Write-Host "Press 'R' to Restart Computer"
+    Write-Host "Press '1' for Reinstall (Settings file will reset)"
+    Write-Host "Press '2' for Repair (Settings file will be kept)"
     Write-Host "Press 'Q' to Quit`n"
 }
 
@@ -24,13 +16,32 @@ do
      $selection = Read-Host "Please make a selection"
      switch ($selection)
      {
-         '1' { Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vipe7/Vipers/master/Update.ps1')) } 
-         '2' { Start-Process "C:\Rainmeter\FPS-FIX" }
-         '3' { Write-Host "Guide is Under Construction" | pause }
-         '4' { Write-Host "Guide is Under Construction" | pause }
-         '5' { Write-Host "Guide is Under Construction" | pause }
-	 '6' { Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vipe7/Vipers/master/WebNowPlaying.ps1')) }
-         'r' { Restart-Computer }
+         '1' { Stop-Process -Name "Rainmeter"
+curl -o $env:userprofile\desktop\Install.zip https://github.com/vipe7/Vipers/raw/master/Vipers.zip
+Remove-Item C:\Rainmeter\* -Force -Recurse
+Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\
+Remove-Item $env:userprofile\desktop\Install.zip -Force -Recurse
+Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
+(get-item C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua).Attributes += 'Hidden'
+(get-item C:\Rainmeter\Checker.exe).Attributes += 'Hidden'
+Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt | Out-String
+Pause
+Exit } 
+         '2' { New-Item -ItemType "directory" -Path "$env:userprofile\desktop\Vipers-Backup" -Force | Out-Null
+Copy-Item -Path "C:\Rainmeter\Skins\Vipers\@Resources\*txt" -Destination "$env:userprofile\desktop\Vipers-Backup"
+Stop-Process -Name "Rainmeter"
+curl -o $env:userprofile\desktop\Install.zip https://github.com/vipe7/Vipers/raw/master/Vipers.zip
+Remove-Item C:\Rainmeter\* -Force -Recurse
+Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\
+Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\*txt" -Destination "C:\Rainmeter\Skins\Vipers\@Resources"
+Remove-Item $env:userprofile\desktop\Vipers-Backup -Force -Recurse
+Remove-Item $env:userprofile\desktop\Install.zip -Force -Recurse
+Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
+(get-item C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua).Attributes += 'Hidden'
+(get-item C:\Rainmeter\Checker.exe).Attributes += 'Hidden'
+Get-Content -Path C:\Rainmeter\Skins\Vipers\DownloadFile\ChangeLog.txt | Out-String
+Pause
+Exit }
          'q' { Exit }
   }
  }
