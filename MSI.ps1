de53e025651164b7bd0c6e0c6401ad0d43f5f33e
vipe7 <bukoreshtliev@gmail.com>
2020-07-31 02:51:06 +0300

    Create MSI.ps1

---
+++ MSI.ps1
@@ -0,0 +1,12 @@
+Write-Host "Vipers Installer`n`n`n`n`n`n`n"
+Write-Host "Downloading MSIAfterburner & RTSS...`n"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/vipe7/Vipers/master/MSI.exe" -OutFile "$env:userprofile\desktop\MSI.exe"
+Write-Host "MSIAfterburner & RTSS downloaded`n"
+Write-Host "Installing MSIAfterburner & RTSS...`n"
+Start-Process -Wait -FilePath $env:userprofile\desktop\MSI.exe -ArgumentList '/S','/v','/qn' -passthru
+Write-Host "MSIAfterburner & RTSS installed`n"
+Write-Host "Removing downloaded files...`n"
+Remove-Item "$env:userprofile\desktop\MSI.exe"
+Remove-Item "$env:userprofile\desktop\MSI Afterburner.lnk"
+Write-Host "ALL DONE`nPlease make MSIAfterburner and RTSS to start with Windows`nCheck step 2) Setting up MSI & TRSS"
+Pause
