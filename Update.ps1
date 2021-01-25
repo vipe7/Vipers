c87e0680bbb20fd3496021a2c68334670a7d0d19
vipe7 <bukoreshtliev@gmail.com>
2021-01-26 00:15:08 +0200

    Update Update.ps1

--- Update.ps1
+++ Update.ps1
@@ -1,10 +1,12 @@
 New-Item -ItemType "directory" -Path "$env:userprofile\desktop\Vipers-Backup" -Force | Out-Null
 Copy-Item -Path "C:\Rainmeter\Skins\Vipers\@Resources\*txt" -Destination "$env:userprofile\desktop\Vipers-Backup"
+Copy-Item -Path "C:\Rainmeter\Rainmeter.ini" -Destination "$env:userprofile\desktop\Vipers-Backup"
 Stop-Process -Name "Rainmeter"
 curl -o $env:userprofile\desktop\Install.zip https://github.com/vipe7/Vipers/raw/master/Vipers.zip
 Remove-Item C:\Rainmeter\* -Force -Recurse
 Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\
 Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\*txt" -Destination "C:\Rainmeter\Skins\Vipers\@Resources"
+Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\Rainmeter.ini" -Destination "C:\Rainmeter"
 Remove-Item $env:userprofile\desktop\Vipers-Backup -Force -Recurse
 Remove-Item $env:userprofile\desktop\Install.zip -Force -Recurse
 Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
