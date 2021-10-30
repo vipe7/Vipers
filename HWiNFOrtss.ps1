84bf29c0017286546b4214f42c25c7743c4aed01
vipe7 <bukoreshtliev@gmail.com>
2021-10-31 02:33:54 +0300

    Update HWiNFOrtss.ps1

--- HWiNFOrtss.ps1
+++ HWiNFOrtss.ps1
@@ -1,7 +1,8 @@
 Stop-Process -Name "RTSS"
 Stop-Process -Name "HWiNFO64"
-Copy-Item -Path "C:\Rainmeter\Plugins\Config" -Destination "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles"
-$1 = New-ScheduledTaskAction -Execute "C:\Rainmeter\Plugins\HWiNFO\HWiNFO64.EXE"
+Unregister-ScheduledTask -TaskName HWiNFO -Confirm:$false
+Unregister-ScheduledTask -TaskName RTSS -Confirm:$false
+$1 = New-ScheduledTaskAction -Execute "C:\Program Files\HWiNFO64\HWiNFO64.EXE"
 $2 = New-ScheduledTaskTrigger -AtLogOn
 $3 = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users" -RunLevel Highest
 $4 = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -ExecutionTimeLimit 0
@@ -14,5 +15,11 @@ $14 = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBa
 $15 = New-ScheduledTask -Action $11 -Principal $13 -Trigger $12 -Settings $14
 Register-ScheduledTask RTSS -InputObject $15
 Set-ScheduledTask -TaskName "\HWiNFO" -Trigger (New-JobTrigger -AtLogOn -RandomDelay (new-Timespan -Seconds 15))
+curl -o "$env:userprofile\desktop\HWiNFO64.INI" "https://github.com/vipe7/Vipers/raw/master/HWiNFO64.INI"
+Copy-Item -Path "$env:userprofile\desktop\HWiNFO64.INI" -Destination "C:\Program Files\HWiNFO64"
+Remove-Item $env:userprofile\desktop\HWiNFO64.INI -Force -Recurse
+curl -o "$env:userprofile\desktop\Config" "https://github.com/vipe7/Vipers/raw/master/Config"
+Copy-Item -Path "$env:userprofile\desktop\Config" -Destination "C:\Program Files (x86)\RivaTuner Statistics Server\Profiles"
+Remove-Item $env:userprofile\desktop\Config -Force -Recurse
 Start-Process -FilePath "RTSS.exe" -WorkingDirectory "C:\Program Files (x86)\RivaTuner Statistics Server"
-Start-Process -FilePath "HWiNFO64.EXE" -WorkingDirectory "C:\Rainmeter\Plugins\HWiNFO"
+Start-Process -FilePath "HWiNFO64.EXE" -WorkingDirectory "C:\Program Files\HWiNFO64"
