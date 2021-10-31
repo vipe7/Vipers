9a8d76161db5eaee88ede47e9d2ef700b2b0911a
vipe7 <bukoreshtliev@gmail.com>
2021-11-01 00:24:14 +0200

    Update RemoveOld.ps1

--- RemoveOld.ps1
+++ RemoveOld.ps1
@@ -7,8 +7,8 @@ New-Item -ItemType "directory" -Path "$env:userprofile\desktop\Vipers-Backup" -F
 Copy-Item -Path "C:\Rainmeter\Skins\Vipers\@Resources\Settings.txt" -Destination "$env:userprofile\desktop\Vipers-Backup"
 Remove-Item C:\Rainmeter -Force -Recurse
 curl -o $env:userprofile\desktop\Rainmeter-4.5.4.exe https://builds.rainmeter.net/Rainmeter-4.5.4.exe
+Start-Process -FilePath "$env:userprofile\desktop\Rainmeter-4.5.4.exe"
 while ((get-process -name Rainmeter -ErrorAction SilentlyContinue).count -eq 0){}
-Start-Sleep -s 3
 Stop-Process -Name "Rainmeter"
 Remove-Item $env:userprofile\Documents\Rainmeter\Skins\illustro -Force -Recurse
 curl -o "$env:userprofile\desktop\Vipers.rmskin" "https://github.com/vipe7/Vipers/raw/master/Vipers.rmskin"
