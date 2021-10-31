dccbc8752c4f48d98f3a2fc2e9bbfc1dff8b857d
vipe7 <bukoreshtliev@gmail.com>
2021-11-01 00:24:38 +0200

    Update RemoveOld.ps1

--- RemoveOld.ps1
+++ RemoveOld.ps1
@@ -9,6 +9,7 @@ Remove-Item C:\Rainmeter -Force -Recurse
 curl -o $env:userprofile\desktop\Rainmeter-4.5.4.exe https://builds.rainmeter.net/Rainmeter-4.5.4.exe
 Start-Process -FilePath "$env:userprofile\desktop\Rainmeter-4.5.4.exe"
 while ((get-process -name Rainmeter -ErrorAction SilentlyContinue).count -eq 0){}
+Start-Sleep -s 3
 Stop-Process -Name "Rainmeter"
 Remove-Item $env:userprofile\Documents\Rainmeter\Skins\illustro -Force -Recurse
 curl -o "$env:userprofile\desktop\Vipers.rmskin" "https://github.com/vipe7/Vipers/raw/master/Vipers.rmskin"
