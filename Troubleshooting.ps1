294d35429dfb5ae1c28fcb361159171d9ffd527b
vipe7 <bukoreshtliev@gmail.com>
2021-11-06 02:44:33 +0200

    Update Troubleshooting.ps1

--- Troubleshooting.ps1
+++ Troubleshooting.ps1
@@ -5,8 +5,8 @@ function Show-Menu
     )
     Clear-Host
     Write-Host "================ $Title ================"
-    Write-Host "Press '1' for Reinstall (Settings files will reset)"
-    Write-Host "Press '2' for Repair (Settings files will be kept)"
+    Write-Host "Press '1' for Completely Reinstall"
+    Write-Host "Press '2' for Repair (Settings file will be kept)"
     Write-Host "Press 'Q' to Quit`n"
 }
 
@@ -17,35 +17,27 @@ do
      switch ($selection)
      {
          '1' { Stop-Process -Name "Rainmeter"
-curl -o $env:userprofile\desktop\Install.zip https://github.com/vipe7/Vipers/raw/master/Vipers.zip
-Remove-Item C:\Rainmeter\* -Force -Recurse
-Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\
-Remove-Item $env:userprofile\desktop\Install.zip -Force -Recurse
-Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
-(get-item C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua).Attributes += 'Hidden'
-$objShell = New-Object -ComObject ("WScript.Shell")
-$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\Vipers.lnk")
-$objShortCut.TargetPath="C:\Rainmeter\Rainmeter.exe"
-$objShortCut.Save()
+curl -o $env:userprofile\desktop\Vipers.rmskin https://github.com/vipe7/Vipers/raw/master/Vipers.rmskin
+Invoke-Item "$env:userprofile\desktop\Vipers.rmskin"
+Write-Host "`nPress install on a poped window`nWaiting..."
+while ((get-process -name Rainmeter -ErrorAction SilentlyContinue).count -eq 0){}
 Write-Host "Vipers is now Reinstalled`nIf your problem is not fixed,`nplease ask for help on discord"
 Pause
 Exit } 
-         '2' { New-Item -ItemType "directory" -Path "$env:userprofile\desktop\Vipers-Backup" -Force | Out-Null
-Copy-Item -Path "C:\Rainmeter\Skins\Vipers\@Resources\*txt" -Destination "$env:userprofile\desktop\Vipers-Backup"
-Copy-Item -Path "C:\Rainmeter\Rainmeter.ini" -Destination "$env:userprofile\desktop\Vipers-Backup"
-Stop-Process -Name "Rainmeter"
-curl -o $env:userprofile\desktop\Install.zip https://github.com/vipe7/Vipers/raw/master/Vipers.zip
-Remove-Item C:\Rainmeter\* -Force -Recurse
-Expand-Archive -Force $env:userprofile\desktop\Install.zip C:\
-Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\*txt" -Destination "C:\Rainmeter\Skins\Vipers\@Resources"
-Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\Rainmeter.ini" -Destination "C:\Rainmeter"
+         '2' { Stop-Process -Name "Rainmeter"
+New-Item -ItemType "directory" -Path "$env:userprofile\desktop\Vipers-Backup" -Force | Out-Null
+Copy-Item -Path "$env:userprofile\Documents\Rainmeter\Skins\Vipers\@Resources\Settings.txt" -Destination "$env:userprofile\desktop\Vipers-Backup"
+curl -o "$env:userprofile\desktop\Vipers.zip" "https://github.com/vipe7/Vipers/raw/master/VipersNEW.zip"
+Remove-Item "$env:userprofile\Documents\Rainmeter\Skins\Vipers\*" -Force -Recurse
+Expand-Archive -Force "$env:userprofile\desktop\Vipers.zip" "$env:userprofile\Documents\Rainmeter\Skins"
+Copy-Item -Path "$env:userprofile\desktop\Vipers-Backup\Settings.txt" -Destination "$env:userprofile\Documents\Rainmeter\Skins\Vipers\@Resources"
 Remove-Item $env:userprofile\desktop\Vipers-Backup -Force -Recurse
-Remove-Item $env:userprofile\desktop\Install.zip -Force -Recurse
-Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Rainmeter"
-(get-item C:\Rainmeter\Skins\Vipers\@Resources\vipe7.lua).Attributes += 'Hidden'
+Remove-Item $env:userprofile\desktop\Vipers.zip -Force -Recurse
+Start-Process -FilePath "Rainmeter.exe" -WorkingDirectory "C:\Program Files\Rainmeter"
+(get-item $env:userprofile\Documents\Rainmeter\Skins\Vipers\@Resources\vipe7.lua).Attributes += 'Hidden'
 $objShell = New-Object -ComObject ("WScript.Shell")
 $objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\Vipers.lnk")
-$objShortCut.TargetPath="C:\Rainmeter\Rainmeter.exe"
+$objShortCut.TargetPath="C:\Program Files\Rainmeter\Rainmeter.exe"
 $objShortCut.Save()
 Write-Host "Vipers is now Repaired`nIf your problem is not fixed,`nplease Reinstall or ask for help on discord"
 Pause
